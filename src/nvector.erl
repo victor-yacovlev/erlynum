d001bbf0daa10c3dc05f51137e7b2c81e25b40c3
Victor Yacovlev <v.yacovlev@gmail.com>
2017-05-31 17:15:30 +0300

    NVector EUnit tests for zeros, ones and full

--- src/nvector.erl
+++ src/nvector.erl
@@ -13,6 +13,9 @@
 %% @end
 
 -module(nvector).
+-include_lib("eunit/include/eunit.hrl").
+-include("erlynum.hrl").
+
 -export([full/2, full/3, zeros/1, zeros/2, ones/1, ones/2]).
 -export([range/1, range/2, range/3, range/4]).
 -export([linspace/3, linspace/4, logspace/3, logspace/4, logspace/5, geomspace/3, geomspace/4]).
@@ -279,3 +282,57 @@ dot(Y, X) -> dot(Y, X, []).
 ) -> erlynum:nscalar().
 %% @doc Returns the inner product of two vectors.
 dot(Y, X, Options) -> ?WE(erlynum_nif:nvector_dot(Y, X, Options)).
+
+% --------------- EUnit testing functions
+
+zeros_empty_test() ->
+    NVector = zeros(0),
+    {RowsCount, ColsCount} = NVector#nvector.shape,
+    TotalCount = RowsCount * ColsCount,
+    ?assertEqual(0, TotalCount),
+    BinData = NVector#nvector.data,
+    ?assertEqual(0, byte_size(BinData)),
+    ?assertEqual(0, NVector#nvector.view#view_params.size).
+
+zeros_nonempty_test() ->
+    Zeros = zeros(5),
+    ?assertEqual([0.0, 0.0, 0.0, 0.0, 0.0], to_list(Zeros)),
+    ?assertEqual([0, 0, 0, 0, 0], to_list(Zeros, integer)),
+    ComplexZeros = zeros(5, [{dtype, c}]),
+    ?assertEqual([{0.0,0.0}, {0.0,0.0}, {0.0,0.0}, {0.0,0.0}, {0.0,0.0}], to_list(ComplexZeros)),
+    ?assertEqual([0, 0, 0, 0, 0], to_list(ComplexZeros, integer)).
+
+ones_empty_test() ->
+    NVector = ones(0),
+    {RowsCount, ColsCount} = NVector#nvector.shape,
+    TotalCount = RowsCount * ColsCount,
+    ?assertEqual(0, TotalCount),
+    BinData = NVector#nvector.data,
+    ?assertEqual(0, byte_size(BinData)),
+    ?assertEqual(0, NVector#nvector.view#view_params.size).
+
+ones_nonempty_test() ->
+    Ones = ones(5),
+    ?assertEqual([1.0, 1.0, 1.0, 1.0, 1.0], to_list(Ones)),
+    ?assertEqual([1, 1, 1, 1, 1], to_list(Ones, integer)),
+    ComplexOnes = ones(5, [{dtype, c}]),
+    ?assertEqual([{1.0,0.0}, {1.0,0.0}, {1.0,0.0}, {1.0,0.0}, {1.0,0.0}], to_list(ComplexOnes)),
+    ?assertEqual([1, 1, 1, 1, 1], to_list(ComplexOnes, integer)).
+
+full_empty_test() ->
+    NVector = full(0, 123),
+    {RowsCount, ColsCount} = NVector#nvector.shape,
+    TotalCount = RowsCount * ColsCount,
+    ?assertEqual(0, TotalCount),
+    BinData = NVector#nvector.data,
+    ?assertEqual(0, byte_size(BinData)),
+    ?assertEqual(0, NVector#nvector.view#view_params.size).
+
+full_nonempty_test() ->
+    Full = full(5, 123),
+    ?assertEqual([123.0, 123.0, 123.0, 123.0, 123.0], to_list(Full)),
+    ?assertEqual([123, 123, 123, 123, 123], to_list(Full, integer)),
+    ComplexFull = full(3, {123,456}),
+    ?assertEqual([{123.0,456.0}, {123.0,456.0}, {123.0,456.0}], to_list(ComplexFull)),
+    ?assertEqual([123.0, 123.0, 123.0], to_list(ComplexFull, real)),
+    ?assertEqual([123, 123, 123], to_list(ComplexFull, integer)).
